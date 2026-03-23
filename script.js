ef3e5e1f1a9400ab3804868ba0fee453eaf86823
qmsqc <qmsqclead@gmail.com>
2026-03-23 13:50:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function verify(){
+
+  var id = document.getElementById("cert").value.trim().toUpperCase();
+
+  var data = {
+    "Q-2025-16401": {
+      name: "ANANTHA PRABU K",
+      course: "Lead Auditor",
+      issueDate: "19/03/2026",
+      status: "Valid"
+    }
+  };
+
+  if(data[id]){
+    document.getElementById("result").innerHTML =
+
+    "<h3 style='color:green;'>Certificate Verified ✅</h3>" +
+
+    "<div class='result-card'>" +
+
+    "<div class='box'><strong>Name:</strong><br>" + data[id].name + "</div>" +
+
+    "<div class='box'><strong>Course:</strong><br>" + data[id].course + "</div>" +
+
+    "<div class='box'><strong>Certificate Number:</strong><br>" + id + "</div>" +
+
+    "<div class='box'><strong>Issue Date:</strong><br>" + data[id].issueDate + "</div>" +
+
+    "<div class='box'><strong>Status:</strong><br>" + data[id].status + "</div>" +
+
+    "</div>";
+
+  }
+  else{
+    document.getElementById("result").innerHTML = 
+    "<span style='color:red;'>Invalid Certificate ❌</span>";
+  }
+
+}

31b7baf8e5d2b658e11974b718da8c86bc34cac5
qmsqc <qmsqclead@gmail.com>
2026-03-23 12:36:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -13,15 +13,27 @@ function verify(){
 
   if(data[id]){
     document.getElementById("result").innerHTML =
-    "Certificate Verified ✅ <br><br>" +
-    "Name: " + data[id].name + "<br>" +
-    "Course: " + data[id].course + "<br>" +
-    "Certificate Number: " + id + "<br>" +
-    "Issue Date: " + data[id].issueDate + "<br>" +
-    "Status: " + data[id].status;
+
+    "<h3 style='color:green;'>Certificate Verified ✅</h3>" +
+
+    "<div class='result-card'>" +
+
+    "<div class='box'><strong>Name:</strong><br>" + data[id].name + "</div>" +
+
+    "<div class='box'><strong>Course:</strong><br>" + data[id].course + "</div>" +
+
+    "<div class='box'><strong>Certificate Number:</strong><br>" + id + "</div>" +
+
+    "<div class='box'><strong>Issue Date:</strong><br>" + data[id].issueDate + "</div>" +
+
+    "<div class='box'><strong>Status:</strong><br>" + data[id].status + "</div>" +
+
+    "</div>";
+
   }
   else{
-    document.getElementById("result").innerHTML = "Invalid Certificate ❌";
+    document.getElementById("result").innerHTML = 
+    "<span style='color:red;'>Invalid Certificate ❌</span>";
   }
 
 }

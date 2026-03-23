eadffa60aa748d48b001176437ba26ef3c2b7b84
qmsqc <qmsqclead@gmail.com>
2026-03-23 10:44:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,27 @@
 function verify(){
 
-  var number = document.getElementById("cert").value
-                .replace(/\s+/g, '')
-                .toUpperCase();
+  var id = document.getElementById("cert").value.trim().toUpperCase();
 
-  if(number === "Q-2025-16401"){
-    document.getElementById("result").innerHTML = "Certificate Verified ✅";
-  } 
+  var data = {
+    "Q-2025-16401": {
+      name: "ANANTHA PRABU K",
+      course: "Lead Auditor",
+      issueDate: "19/03/2026",
+      status: "Valid"
+    }
+  };
+
+  if(data[id]){
+    document.getElementById("result").innerHTML =
+    "Certificate Verified ✅ <br><br>" +
+    "Name: " + data[id].name + "<br>" +
+    "Course: " + data[id].course + "<br>" +
+    "Certificate Number: " + id + "<br>" +
+    "Issue Date: " + data[id].issueDate + "<br>" +
+    "Status: " + data[id].status;
+  }
   else{
     document.getElementById("result").innerHTML = "Invalid Certificate ❌";
   }
-
-}
-function verify(){
-
-var id = document.getElementById("cert").value.trim();
-
-var data = {
-  "Q-2025-16401": {
-    name: "ANANTHA PRABU K",
-    course: "Lead Auditor",
-    issueDate: "19/03/2026",
-    status: "Valid"
-  },
-};
-
-if(data[id]){
-document.getElementById("result").innerHTML =
-"Certificate Verified ✅ <br><br>" +
-"Name: " + data[id].name + "<br>" +
-"Course: " + data[id].course + "<br>" +
-"Certificate Number: " + id + "<br>" +
-"Issue date: " + data[id].issueDate + "<br>" +
-"Status: " + data[id].status;
-}
-else{
-document.getElementById("result").innerHTML="Invalid Certificate ❌";
-}
 
 }
